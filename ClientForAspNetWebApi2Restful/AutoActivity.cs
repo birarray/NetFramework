ee09141bd567a33f842150c518104652b65afd4f
birarray <101454768+birarray@users.noreply.github.com>
2022-07-18 00:10:33 +0900

    Client for Restful

---
+++ ClientForAspNetWebApi2Restful/AutoActivity.cs
@@ -0,0 +1,99 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AndroidX.AppCompat.App;
+using System.Net.Http;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace ClientForAspNetWebApi2Restful
+{
+    [Activity(Label = "AutoActivity", Theme = "@style/AppTheme")]
+    public class AutoActivity : AppCompatActivity
+    {
+        EditText modelBox;
+        EditText producedBox;
+        Button buttonDel;
+        Button buttonSave;
+        Guid autoId = Guid.Empty;
+        HttpClient _client = new HttpClient();
+        string UrlWebApi = "http://192.168.1.38/AndroidServer";
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.activity_auto);
+            modelBox = FindViewById<EditText>(Resource.Id.model);
+            producedBox = FindViewById<EditText>(Resource.Id.produced);
+            buttonDel = FindViewById<Button>(Resource.Id.buttonDel);
+            buttonDel.Click += (o, e) => { delete(); };
+            buttonSave = FindViewById<Button>(Resource.Id.buttonSave);
+            buttonSave.Click += (o, e) => { save(); };
+            Bundle extras = Intent.Extras;
+            if (extras != null)
+            {
+                autoId = Guid.Parse(extras.GetString("id"));
+            }
+            
+            if (autoId != Guid.Empty)
+            {
+                getData(autoId.ToString());
+            }
+            else
+            {
+                //Скрываем кнопку удаления
+                button.Visibility = ViewStates.Gone;
+            }
+            _client.Timeout = new TimeSpan(0, 0, 300);
+        }
+
+        async void getData(string constraint)
+        {
+            // Получение данных при помощи сервиса
+            var response = await _client.GetAsync(string.Format(UrlWebApi + "/api/Auto/GetAllData/?constraint={0}", HttpUtility.UrlEncode(constraint)));
+            var obj = await response.Content.ReadAsStringAsync();
+            var cars = JsonConvert.DeserializeObject<List<DataCar>>(obj);
+            if (cars.Count > 0)
+            {
+                modelBox.Text = cars[0].model.ToString();
+                producedBox.Text = cars[0].produced.ToString();
+            }
+        }
+        async void save
+        {
+            int_produced = 0;
+            if (!int.TryParse(producedBox.Text, out _produced)) return;
+            if (autoId != Guid.Empty)
+            {
+                var response = await _client.GetAsync(string.Format(UrlWebApi + "/api/Auto/GetEditData/?Id={0}&Model={1}&Produced={2}", autoId.ToString(), HttpUtility.UrlEncode(modelBox.Text), _produced.ToString()));
+            }
+            else
+            {
+               var response = await _client.GetAsync(string.Format(UrlWebApi + "/api/Auto/GetAddData/?Model={0}&Produced={1}", HttpUtility.UrlEncode(modelBox.Text), _produced.ToString()));
+            }
+            goBack();
+
+        }
+        async void delete()
+        {
+        var response = await _client.GetAsync(string.Format(UrlWebApi + "/api/Auto/GetDeleteData/?Id={0}", autoId.ToString()));
+            goBack();
+        }
+        private void goBack()
+        {
+            // Переход к главной Activity
+            Intent intent = new Intent(this.ToString typeof(MainActivity));
+            intent.AddFlags(ActivityFlags.ClearTop | ActivityFlags.SingleTop);
+            StartActivity(intent);
+        }
+
+    }
+}
---
+++ ClientForAspNetWebApi2Restful/MainActivity.cs
@@ -0,0 +1,140 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using Android.Widget;
+using Android.Content;
+using System;
+using System.Data;
+using System.Linq;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web;
+using Newtonsoft.Json;
+
+
+namespace ClientForAspNetWebApi2Restful
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        ListView autoList;
+        ArrayAdapter autoAdapter;
+        HttpClient _client = new HttpClient();
+        string UrlWebApi = "http://192.168.1.38/AndroidServer";
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            autoList = FindViewById<ListView>(Resource.Id.list);
+            var btn = FindViewById<Button>(Resource.Id.btn);
+            btn.Click += (o, e) =>
+            {
+                Intent intent = new Intent(ApplicationContext, typeof(AutoActivity));
+                StartActivity(intent);
+            };
+            autoList.ItemClick += (o, e) =>
+            {
+                Auto selectedCar = (Auto)e.Parent.GetItemAtPosition(e.Position);
+                Intent intent = new Intent(ApplicationContext, typeof(AutoActivity));
+                intent.PutExtra("id", selected.id);
+                StartActivity(intent);
+            };
+            _client.Timeout = new TimeSpan(0, 0, 300);
+            createDB();
+        }
+            async void createDB()
+            { 
+            try
+            {
+                var response = await _client.GetAsync(string.Format(UrlWebApi + "/api/Auto/GetBaseXML"));
+            }
+            catch (Exception err)
+            {
+                Toast.MakeText(this, err.Message, ToastLength.Long).Show();
+            }
+            }
+        protected override void OnResume()
+        {
+            base.OnResume();
+            GetDatabasePath(string.Empty);
+        }
+        async void getData(string constraint)
+        {
+            try
+            {
+                // Получение данных при помощи сервиса
+                var response = await _client.GetAsync(string.Format(UrlWebApi + "/api/Auto/GetAllData/?constraint={0}", HttpUtility.UrlEncode(constraint)));
+
+                var obj = await response.Content.ReadAsStringAsync();
+                var _data = JsonConvert.DeserializeObject<List<DataCar>>(obj);
+                if (_data.Count > 0)
+                {
+                    List<Auto> cars = new List<Auto>(_data.Select(m => new Auto()
+                    {
+                        id = m.id,
+                        model = m.model,
+                        produced = m.Produced
+                    }));
+                    // Создаем адаптер, передаем в него список
+                    var autoAdapter = new AutoAdapter(this, this, Resource.Layout.list_item, cars);
+                    autoList.Adapter = autoAdapter;
+                }
+                else
+                {
+                    autoList.Adapter = null;
+                }
+            }
+            catch (Exception err)
+            {
+                Toast.MakeText(this, err.Message, ToastLength.Long).Show();
+            }
+        }
+    }
+    public class DataCar
+
+    {
+        public string id { get; set; } // идентификатор
+        public string model { get; set; } // модель
+        public int produced { get; set; } // год выпуска
+    }
+    public class Auto: Java.Lang.Object
+
+    {
+        public string id { get; set; } // идентификатор
+        public string model { get; set; } // модель
+        public int produced { get; set; } // год выпуска
+    }
+
+    public class AutoAdapter : ArrayAdapter
+    {
+        private Activity activity;
+        public AutoAdapter(Activity activity, Context context, int resource, List<Auto> objects) : base(context, resource, objects)
+        {
+            this.activity = activity;
+        }
+        public override Android.Views.View GetView(int position, Android.Views.View convertView, Android.Views.ViewGroup parent)
+        {
+            var view = (convertView ?? activity.LayoutInflater.Inflate(Resource.Layout.list_item, parent, false)) as LinearLayout;
+            TextView modelView = view.FindViewById<TextView>(ResourceCursorAdapter.Id.model);
+            TextView producedView = view.FindViewById<TextView>(ResourceCursorAdapter.Id.produced);
+            var item = (Auto)this.GetItem(position);
+            modelView.Text = item.model;
+            producedView.Text = item.produced.ToString();
+            return view;
+        }
+    }
+}
+/*
+    public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
+*/
